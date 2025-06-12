cafe01473606545e3f196d79788bd62684e3d149
SashaXser <24498484+SashaXser@users.noreply.github.com>
2025-06-12 03:19:32 +0000

    Упрощение логики остановки прослушивания и удаление лишнего метода

--- src/server/stt/system.py
+++ src/server/stt/system.py
@@ -83,26 +83,22 @@ class SttSystem:
             self._do_stop_listening()
 
     def _do_stop_listening(self):
-        self._finalize_stop_listening()
+        self._is_listening = False
+        self.stop_listening_at_time = None
+
+        self._backend.stop_listening()
+        self._producer.produce_event(Event(data=EventDataFromServer.SttStopListening(type='stt_stop_listening')))
 
     def _handle_cancel_listening(self):
         if not self._is_listening:
             return
 
         self._is_cancelled = True
-        self._extracted_from__handle_cancel_listening_2()
-
-    def _finalize_stop_listening(self):
         self._is_listening = False
         self.stop_listening_at_time = None
+
         self._backend.stop_listening()
-        self._producer.produce_event(
-            Event(
-                data=EventDataFromServer.SttStopListening(
-                    type='stt_stop_listening'
-                )
-            )
-        )
+        self._producer.produce_event(Event(data=EventDataFromServer.SttStopListening(type='stt_stop_listening')))
 
     def _create_backend(self) -> AbstractSttBackend:
         system = self._config.system
